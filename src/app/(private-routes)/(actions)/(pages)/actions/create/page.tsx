a9cc8d9f3a1c654dbeb2f5140294c303134b2058
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-08-08 10:10:57 -0300

    refactor: Create ActionsCreate page component

---
+++ src/app/(private-routes)/(actions)/(pages)/actions/create/page.tsx
@@ -0,0 +1,12 @@
+import PageContainer from "@/components/layout/PageContainer";
+import React from "react";
+
+const ActionsCreate = () => {
+  return (
+    <PageContainer title='Crie Sua Ong'>
+      <div className='flex flex-grow flex-col gap-4'></div>
+    </PageContainer>
+  );
+};
+
+export default ActionsCreate;
