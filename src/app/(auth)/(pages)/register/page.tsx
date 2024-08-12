98bf4941f85666a2edf653a60fcc1431e1c1bcfa
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-08-11 21:52:53 -0300

    refactor: Optimize imports in Register page component

--- src/app/(auth)/(pages)/register/page.tsx
+++ src/app/(auth)/(pages)/register/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+// Next
 import Image from "next/image";
 import Link from "next/link";
+// Components
 import { RegisterForm } from "../../components/forms/RegisterForm";
 
 const Register = () => {
