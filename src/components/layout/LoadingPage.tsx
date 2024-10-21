c68a0f590282f993482099505c0aae9c9d11deac
Luis Henrique <luisdev9225@gmail.com>
2024-10-21 03:03:14 -0300

    Refactor LoadingPage component to remove unnecessary text

--- src/components/layout/LoadingPage.tsx
+++ src/components/layout/LoadingPage.tsx
@@ -5,7 +5,6 @@ import Loading from "@/app/loading";
 function LoadingPage() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center'>
-      <div className='pb-4'>Just give us a second.</div>
       <div className='flex items-center justify-center'>
         <Loading />
       </div>
