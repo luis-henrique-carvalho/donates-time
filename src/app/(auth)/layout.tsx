c5bc77e2fa772825131d990154255b810467339a
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-10-26 00:30:34 -0300

    refactor: Remove unused code in PrivateLayout component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,9 @@
-import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
-import nextAuthOptions from "@/nextAuthOptions";
-import { redirect } from "next/navigation";
 
 interface PrivateLayoutProps {
   children: ReactNode;
 }
 
 export default async function PrivateLayout({ children }: PrivateLayoutProps) {
-  const session = await getServerSession(nextAuthOptions);
-
-  if (session) {
-    redirect("/");
-  }
-
   return <>{children}</>;
 }
